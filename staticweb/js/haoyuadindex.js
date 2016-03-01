05c96a66a6f35de9ec7dd2afb5c2b7941fe02a35
SUT-GC <gouchao2008@qq.com>
2016-03-01 19:30:17 +0800

    完成导航栏

---
+++ staticweb/js/haoyuadindex.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	//导航栏pading-left的宽度
+	winwidth = $(window).width()/2
+	
+	if(winwidth - 420 < 0){
+		winwidth = 0;
+	}else{
+		winwidth = winwidth - 420;
+	}
+	$('.nav').css('margin-left',winwidth);
+
+
+});
