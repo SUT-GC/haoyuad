f47dbcded2d9ff5df31ceae106b47ac142b9e28f
SUT-GC <gouchao2008@qq.com>
2016-03-04 11:26:19 +0800

    完成导航栏的独立js跳转

--- staticweb/js/nav.js
+++ staticweb/js/nav.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	windowwight = $(window).width();
 	$(".wangzhan_headlogo").css("width",windowwight);
-	
+
 	//导航栏pading-left的宽度
 	winwidth = $(window).width()/2
 	
@@ -11,5 +11,30 @@ $(document).ready(function(){
 		winwidth = winwidth - 420;
 	}
 	$('.nav').css('margin-left',winwidth);
+
+	$(".nav").find("a").click(function(){
+		name = this.text;
+		if(name=="网站首页"){
+			window.location.href="index.html"
+		}
+		if(name=="工作流程"){
+			window.location.href="workflow.html"
+		}
+		if(name=="公司简介"){
+
+		}
+		if(name=="经典案例"){
+
+		}
+		if(name=="主创团队"){
+
+		}
+		if(name=="招兵买马"){
+
+		}
+		if(name=="联系我们"){
+
+		}
+	});
 
 });
