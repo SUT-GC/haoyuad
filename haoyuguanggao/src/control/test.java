efd293dd105ae33388b2fffb39d1951bb187a6b4
SUT-GC <gouchao2008@qq.com>
2016-03-18 22:00:13 +0800

    eclipse init

---
+++ haoyuguanggao/src/control/test.java
@@ -0,0 +1,10 @@
+package control;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class test extends ActionSupport {
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
